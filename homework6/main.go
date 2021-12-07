ad2f8e26972b0cdcd3dd170fba40ca26b43c4166
Annette <am.osipenko@gmail.com>
2021-12-07 18:26:44 +0300

    homework6 fixes

--- homework6/main.go
+++ homework6/main.go
@@ -31,6 +31,17 @@ func (list *list) push(value string) {
 	list.lastNode = &newNode
 }
 
+// Разворачивает список в обратную сторону
+func (list *list) reverse() {
+	nodePointer := list.firstNode
+	list.lastNode = nil
+
+	for nodePointer != nil {
+		nodePointer.nextNode, nodePointer, list.lastNode = list.lastNode, nodePointer.nextNode, nodePointer
+	}
+	list.firstNode, list.lastNode = list.lastNode, list.firstNode
+}
+
 func main() {
 	fmt.Println("Введите строки для заполнения списка. Для прекращения ввода используйте EOF")
 	var str string
@@ -42,14 +53,23 @@ func main() {
 		list.push(str)
 	}
 
-	fmt.Println("First: ", list.firstNode)
-	fmt.Println("Last: ", list.lastNode)
+	//fmt.Println("First: ", list.firstNode) // Выводит адрес list.firstNode.nextNode
+	//fmt.Println("Last: ", list.lastNode)   // Выводит адрес list.lastNode.nextNode
+	//fmt.Printf("First: %p\n", list.firstNode)
+	//fmt.Printf("Last: %p\n", list.lastNode)
 
 	// развернуть список
+	list.reverse()
+
 	fmt.Println("Развернутый список:")
 	nodePointer := list.firstNode
 	for nodePointer != nil {
 		fmt.Println(nodePointer.value)
 		nodePointer = nodePointer.nextNode
 	}
+
+	//fmt.Println("First: ", list.firstNode) // Выводит адрес list.firstNode.nextNode
+	//fmt.Println("Last: ", list.lastNode)   // Выводит адрес list.lastNode.nextNode
+	//fmt.Printf("First: %p\n", list.firstNode)
+	//fmt.Printf("Last: %p\n", list.lastNode)
 }
